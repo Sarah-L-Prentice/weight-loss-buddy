9250b4fa11b71b723766a33baa7ae3e94fe3acb9
Sarah-Prentice <prens0022@gmail.com>
2025-06-28 10:30:11 +0100

    Add in graph to main activity screen

--- app/src/main/java/com/prenticeweb/weightlossbuddy/activity/MainActivity.java
+++ app/src/main/java/com/prenticeweb/weightlossbuddy/activity/MainActivity.java
@@ -2,6 +2,8 @@ package com.prenticeweb.weightlossbuddy.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,27 +13,117 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.listener.ChartTouchListener;
+import com.github.mikephil.charting.listener.OnChartGestureListener;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.prenticeweb.weightlossbuddy.R;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, OnChartGestureListener, OnChartValueSelectedListener {
+
+    private static final String TAG = "MainActivity";
+
+    private LineChart chart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-        Button mClickButton1 = findViewById(R.id.button1);
-        mClickButton1.setOnClickListener(this);
+        chart = findViewById(R.id.lineChart);
+        chart.setOnChartGestureListener(this);
+        chart.setOnChartValueSelectedListener(this);
+        chart.setDragEnabled(true);
+        chart.setScaleEnabled(false);
+
+        LimitLine targetWeight = new LimitLine(35f);
+        targetWeight.setLineWidth(4f);
+        targetWeight.enableDashedLine(10f,10f,0f);
+        targetWeight.setTextSize(15f);
+        targetWeight.setLabel("Target Weight");
+
+        ArrayList<Entry> yValues = new ArrayList<>();
+        yValues.add(new Entry(0, 60));
+        yValues.add(new Entry(1, 50));
+        yValues.add(new Entry(2, 70));
+        yValues.add(new Entry(3, 30));
+        yValues.add(new Entry(4, 50));
+        LineDataSet set1 = new LineDataSet(yValues, "Data Set 1");
+        set1.setFillAlpha(110);
+        set1.setLineWidth(3);
+        set1.setValueTextSize(10f);
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(set1);
+        LineData data = new LineData(dataSets);
+        chart.setData(data);
+        String[] values = new String[] {"Jan", "Feb", "March", "April", "May"};
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setGranularity(1);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTH_SIDED);
     }
 
     @Override
     public void onClick(View v) {
         Intent intent = new Intent(this, ScreenSliderPagerActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onChartGestureStart(MotionEvent me, ChartTouchListener.ChartGesture lastPerformedGesture) {
+        Log.i(TAG, "onChartGestureStart: X"  + me.getX() + "Y: " + me.getY());
+    }
+
+    @Override
+    public void onChartGestureEnd(MotionEvent me, ChartTouchListener.ChartGesture lastPerformedGesture) {
+        Log.i(TAG, "onChartGestureEnd: "  + lastPerformedGesture);
+    }
+
+    @Override
+    public void onChartLongPressed(MotionEvent me) {
+        Log.i(TAG, "onChartLongPressed");
+    }
+
+    @Override
+    public void onChartDoubleTapped(MotionEvent me) {
+        Log.i(TAG, "onChartDoubleTapped");
+    }
+
+    @Override
+    public void onChartSingleTapped(MotionEvent me) {
+        Log.i(TAG, "onChartSingleTapped");
+    }
+
+    @Override
+    public void onChartFling(MotionEvent me1, MotionEvent me2, float velocityX, float velocityY) {
+        Log.i(TAG, "onChartFling: velocityX:" + velocityX + " velocityY: " + velocityY);
+    }
+
+    @Override
+    public void onChartScale(MotionEvent me, float scaleX, float scaleY) {
+        Log.i(TAG, "onChartScale: ScaleX: " + scaleX + " ScaleY: " + scaleY);
+    }
+
+    @Override
+    public void onChartTranslate(MotionEvent me, float dX, float dY) {
+        Log.i(TAG, "onChartTranslate: dX: " + dX + " dy: " + dY );
+    }
+
+    @Override
+    public void onValueSelected(Entry e, Highlight h) {
+        Log.i(TAG, "onValueSelected: " + e.toString());
+    }
+
+    @Override
+    public void onNothingSelected() {
+        Log.i(TAG, "onNothingSelected");
     }
 }
